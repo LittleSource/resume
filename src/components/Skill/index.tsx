b929572b49f4ef1af032aab40f37972d4ae75384
LittleSource <444168950@qq.com>
2022-08-16 22:14:39 +0800

    fix .

--- src/components/Skill/index.tsx
+++ src/components/Skill/index.tsx
@@ -14,26 +14,24 @@ export interface SkillProps {
 const Skill: FC<SkillProps> = (props) => {
   return (
     <div className="flex flex-wrap justify-between">
-      {
-        props.data.map((item, index) => {
-          return (
-            <div className="mt-1" key={index}>
-              <div className="text-base font-semibold font-title">
-                {item.title}
-              </div>
-              {item.item.map((text, index) => {
-                return (
-                  <div className="text-base flex items-center" key={index}>
-                    <div className="w-1 h-1 bg-blue-100 mr-1 rounded-full"></div>
-                    {text}
-                  </div>
-                );
-              })}
+      {props.data.map((item, index) => {
+        return (
+          <div className="mt-1" key={index}>
+            <div className="text-base font-semibold font-title">
+              {item.title}
             </div>
-          );
-        })
-      }
-    </div >
+            {item.item.map((text, index) => {
+              return (
+                <div className="flex" key={index}>
+                  <div className="text-blue-100 text-2xl mr-1 leading-6">·</div>
+                  <div className="text-base">{text}</div>
+                </div>
+              );
+            })}
+          </div>
+        );
+      })}
+    </div>
   );
 };
 
