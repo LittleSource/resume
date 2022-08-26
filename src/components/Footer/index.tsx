855bbd171a050348621817a5303c71fe31615603
LittleSource <444168950@qq.com>
2022-08-26 14:17:44 +0800

    update project icon

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,19 +1,19 @@
-import { FC } from "react";
+import { FC } from 'react';
 import './index.less';
 const Footer: FC = () => {
-    return (
-        <div className="footer w-full mt-3 flex items-center justify-center">
-            <a
-                className="text-2xl i-mdi:github"
-                target="_bank"
-                href="https://github.com/LittleSource/resume-react">
-            </a>
-            <a
-                className="text-2xl ml-2 i-mdi:file-pdf-box"
-                onClick={() => window.print()}>
-            </a>
-        </div>
-    )
-}
+  return (
+    <div className="footer w-full mt-3 flex items-center justify-center">
+      <a
+        className="text-2xl i-mdi:github"
+        target="_blank"
+        href="https://github.com/LittleSource/resume-react"
+      ></a>
+      <a
+        className="text-2xl ml-2 i-mdi:file-pdf-box"
+        onClick={() => window.print()}
+      ></a>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
--- src/components/Project/index.tsx
+++ src/components/Project/index.tsx
@@ -1,31 +1,35 @@
 import { FC } from 'react';
-//import { GithubOutlined } from '@ant-design/icons';
-
+// 传入imgUrl则展示项目右侧图片
+// 传入githubUrl则展示项目右侧github的icon
+// 两者不能同时存在
 export interface ProjectProps {
   title: string;
   desc: string;
   imgUrl?: string;
+  githubUrl?: string;
 }
 
 const Project: FC<ProjectProps> = (props: ProjectProps) => {
   return (
     <div className="flex justify-between items-center mt-2 2xl:mt-1">
-      <div className="2xl:w-10/12">
+      <div className="w-11/12">
         <div className="text-base font-semibold font-title">{props.title}</div>
         <div className="text-base">{props.desc}</div>
       </div>
-      {
-        props?.imgUrl && (
-          <img
-            className="flex-shrink h-20 w-20"
-            src={props.imgUrl}
-            alt="小程序码"
-          />
-        )
-        //   (
-        //     <GithubOutlined className="project-icon" />
-        //   )
-      }
+      {props?.imgUrl && (
+        <img
+          className="flex-shrink h-20 w-20"
+          src={props.imgUrl}
+          alt="小程序码"
+        />
+      )}
+      {props?.githubUrl && (
+        <a
+          className="w-10 text-2xl text-blue-100 i-mdi:github"
+          target="_blank"
+          href={props.githubUrl}
+        ></a>
+      )}
     </div>
   );
 };
